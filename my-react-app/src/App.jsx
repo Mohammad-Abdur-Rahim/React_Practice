799d2d2442df4e6a0207f831aa32337004177bb8
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-28 23:14:35 +0600

    Add simple props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,18 +7,17 @@ function App() {
     <>
       
       <h1 className='newcolor'>React Website Project .......</h1>
-     <AddComponent></AddComponent>
+    <Device name="Desktop" price="90000" ></Device>
+    <Device name="Mobile" price="40000" ></Device>
+    <Device name="Tablet" price="20000" ></Device>
+    <Device name="Watch" price="10000" ></Device>
 
     </>
   )
 }
-function AddComponent(){
-  const add ={
-    color:'yellow',
-    border: '1px solid red',
-    padding:'90px'
-  }
-  return <h1 style={add}>Add Somthging React Project </h1>
-}
-
+ function Device(props) {
+  return (
+    <h2>Device is : {props.name} And Price : {props.price} </h2>
+  )
+ }
 export default App
