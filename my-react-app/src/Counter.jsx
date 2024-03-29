c20af9ae1a6ebe8419fe5150f459deedd6473c9a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-29 12:23:34 +0600

    **UseState add/Remove Count Done

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -6,10 +6,16 @@ export default function Counter(){
         const newcount =count+1;
         setcount(newcount)
     }
+    const handleRemove =()=>{
+
+        const newcount =count-1;
+        setcount(newcount)
+    }
     return(
         <div>
         <h3>Count : {count}</h3>
         <button onClick={handleAdd}>Add</button>
+        <button onClick={handleRemove}>Remove</button>
         </div>
     )
 }
