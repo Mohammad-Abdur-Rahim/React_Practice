77acc976b59c87ddb5eb6a73ab6e75fa013d626b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-29 22:01:25 +0600

    ***Countries Data Load Start **

---
+++ react-world-tour/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './components/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Country Tour React</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
