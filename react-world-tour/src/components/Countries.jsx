0daf74a902508c26b4c4a3b35520ad1622749f23
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-30 19:46:50 +0600

    Flag Add console

--- react-world-tour/src/components/Countries.jsx
+++ react-world-tour/src/components/Countries.jsx
@@ -7,16 +7,24 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries,setCountries] =useState([]);
+    const [flag,setFlag]=useState([]);
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data=> setCountries(data))
     },[]);
+   
+    const handleFlag=(flag)=>{
+         console.log("Flag Add")
+    }
     return (
       
         <div className="Countries-container">
             {
-                countries.map(country => <Country key={country.cc3a} country={country}></Country>)
+                countries.map(country => <Country key={country.cc3a} country={country}
+                    handleFlag={handleFlag}
+                ></Country>)
+                
             }
         </div>
     );
--- react-world-tour/src/components/Country.jsx
+++ react-world-tour/src/components/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country,handleFlag}) => {
     const {name,flags,population,area,cca3}=country;
  const [visited,setVisited] = useState(false);
  const handleVisited =()=>{
@@ -15,6 +15,7 @@ const Country = ({country}) => {
             <p>Area:{area}</p>
             <p>Code: {cca3}</p>
             <button onClick={handleVisited}>Visited</button>
+            <button onClick={()=>handleFlag(country.flags.png)}>Add Flag</button>
             {
                 visited?" visited Done":" Pending"
             }
