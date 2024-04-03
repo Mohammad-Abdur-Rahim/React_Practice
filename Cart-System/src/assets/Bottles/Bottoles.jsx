27eca51fd0d98e382c0adce4ef04155a7c375c3e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-04-03 20:30:34 +0600

    Cart grid show done

--- Cart-System/src/assets/Bottles/Bottoles.jsx
+++ Cart-System/src/assets/Bottles/Bottoles.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import Bottle from "../Bottle/Bottle";
+import './Bottoles.css'
 
 const Bottoles = () => {
 const [bottles,setBottles]=useState([]);
@@ -13,9 +14,11 @@ fetch('Bottles.json')
     return (
         <div>
             <h1>Bootles Here : {bottles.length}</h1>
-            {
+           <div className="bottole-container">
+           {
                 bottles.map(bottle =><Bottle key={bottle.id} bottle={bottle}></Bottle>)
             }
+           </div>
         </div>
     );
 };
